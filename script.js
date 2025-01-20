e307717b8b5f85fa3d23801b943e326983113e0a
Yael Ibarra <ibgayael@gmail.com>
2025-01-20 17:45:41 -0600

    Add header

--- script.js
+++ script.js
@@ -25,3 +25,18 @@ function updateCountdown() {
 
 // Run Countdown
 const timerInterval = setInterval(updateCountdown, 1000);
+
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
